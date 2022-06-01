1d6f1581c909c4bde0bc691b1b43f06b81c140de
Victor-Gold <goldvictor137@gmail.com>
2022-06-01 15:14:00 +0100

    commit after completing challenge

--- problems.js
+++ problems.js
@@ -1,90 +1,137 @@
-function divideByThree(num) {
-    /* Returns the passed in number argument divided by three. */
-    // Your code here
-};
+function divideByThree (num) {
+  /* Returns the passed in number argument divided by three. */
+  // Your code here
+  let result = num / 3
+  return result
+}
 
-function averageOfTwo(num1, num2) {
-    /* Returns the average of two numbers, num1 and num2. */
-    // Your code here
-};
+function averageOfTwo (num1, num2) {
+  /* Returns the average of two numbers, num1 and num2. */
+  // Your code here
+  let average = (num1 + num2) / 2
+  return average
+}
 
-function averageOfFour(num1, num2, num3, num4) {
-    /* Takes in four numbers. The function should return the average of all of
+function averageOfFour (num1, num2, num3, num4) {
+  /* Takes in four numbers. The function should return the average of all of
     the numbers. */
-    // Your code here
-};
+  // Your code here
+  let result = (num1 + num2 + num3 + num4) / 4
+  return result
+}
 
-function doubler(nums) {
-    /* Takes an array of numbers and returns a new array where every element of
+function doubler (nums) {
+  /* Takes an array of numbers and returns a new array where every element of
     the original array is multiplied by 2. */
-    // Your code here
-};
+  // Your code here
+  let doubled = nums.map(x => x * 2)
+  return doubled
+}
 
-function combineArrays(arr1, arr2) {
-    /* Takes in two arrays of numbers and returns the two arrays combined into
+function combineArrays (arr1, arr2) {
+  /* Takes in two arrays of numbers and returns the two arrays combined into
     a single array. **Hint**: Use the `Array.concat` method but be aware that
     calling this method won't permanently change, also known as **mutate**,
     either array. */
-    // Your code here
-};
+  // Your code here
+  let newArray = arr1.concat(arr2)
+  return newArray
+}
 
-function wordWithinArray(word, arr) {
-    /* Takes in both a word and an array of words as arguments and returns a
+function wordWithinArray (word, arr) {
+  /* Takes in both a word and an array of words as arguments and returns a
     boolean that returns true if that string is located inside of the array, or
     false if it does not. Use `Array.indexOf`. */
-    // Your code here
-};
+  // Your code here
+  let result = arr.indexOf(word) > -1
+  return result
+}
 
-function echo(str) {
-    /* Takes in a string and returns that string "echo-ized". E.g. 
+function echo (str) {
+  /* Takes in a string and returns that string "echo-ized". E.g. 
     echo("Mom!"); // => returns "MOM! ... Mom! ... mom!"
     echo("hey"); // => returns "HEY ... hey ... hey"
     echo("JUMp"); // => returns "JUMP ... JUMp ... jump" */
-    // Your code here
-};
+  // Your code here
 
-function fizzBuzz(max) {
-    /* Takes a number, max and returns an array that contains every number from
+  let firstString = str.toUpperCase()
+  //   let secondString = str.charAt(0).toUpperCase() + string.slice(1);
+  let secondString = str.toLowerCase()
+  let result = firstString + ' ... ' + str + ' ... ' + secondString
+  return result
+}
+
+function fizzBuzz (max) {
+  /* Takes a number, max and returns an array that contains every number from
     0 to max (not inclusive) that is divisible by either 3 or 5, **but not both**. */
-    // Your code here
-};
+  // Your code here
+  let array = []
+  for (let index = 0; index < max; index++) {
+    if (index % 5 === 0 && index % 3 === 0) {
+      continue
+    } else if (index % 5 === 0) {
+      array.push(index)
+    } else if (index % 3 === 0) {
+      array.push(index)
+    }
+  }
+  return array
+}
 
-function hello(name) {
-    /* Takes in a string name and returns a string saying "Hello, " to that name. */
-    // Your code here
-};
+function hello (name) {
+  /* Takes in a string name and returns a string saying "Hello, " to that name. */
+  // Your code here
+  let result = 'Hello, ' + name
+  return result
+}
 
-function goodbye(name) {
-    /* Takes in a string name and returns a string saying "Bye, " to that name. */
-    // Your code here
-};
+function goodbye (name) {
+  /* Takes in a string name and returns a string saying "Bye, " to that name. */
+  // Your code here
+  let result = 'Bye, ' + name
+  return result
+}
 
-function isFive(num) {
-    /* Takes in a number, num, and returns `true` if a number is equal to 5 and
+function isFive (num) {
+  /* Takes in a number, num, and returns `true` if a number is equal to 5 and
     `false` if it is not. */
-    // Your code here
-};
+  if (num === 5) {
+    return true
+  } else {
+    return false
+  }
+  // Your code here
+}
 
-function isOdd(num) {
-    /* Takes in a number and returns `true` if the number is odd and returns
+function isOdd (num) {
+  /* Takes in a number and returns `true` if the number is odd and returns
     `false` otherwise. Try writing this with and without `if` statements */
-    // Your code here
-};
+  // Your code here
+  if (num % 2 === 0) {
+    return false
+  } else {
+    return true
+  }
+}
 
-function isSubString(searchString, subString) {
-    /* Takes in two strings, `searchString` and `subString`. Should return
+function isSubString (searchString, subString) {
+  /* Takes in two strings, `searchString` and `subString`. Should return
     `true` if `subString` is a part of the`searchString`, regardless of upper
     or lower case, and `false` if otherwise. */
-    // Your code here
-};
+  // Your code here
+  if (searchString.toLowerCase().indexOf(subString.toLowerCase()) > -1) {
+    return true
+  } else {
+    return false
+  }
+}
 
-function aCounter(word) {
-    /* Takes in a word and returns the number of a's within that word. Counts
+function aCounter (word) {
+  /* Takes in a word and returns the number of a's within that word. Counts
     both lowercase (a) and uppercase (A). Your job is to translate the following
     function to use a `for` loop instead of the `while` loop it is currently
     using. */
-
-    /* 
+  /* 
     let index = 0;
     let count = 0;
     while (index < word.length) {
@@ -96,22 +143,30 @@ function aCounter(word) {
     }
     return count;
     */
-    // Your code here
-};
+  // Your code here
+  let count = 0
+  for (let index = 0; index < word.length; index++) {
+    let char = word[index]
+    if (char === 'a' || char === 'A') {
+      count += 1
+    }
+  }
+  return count
+}
 
 module.exports = {
-    divideByThree,
-    averageOfTwo,
-    averageOfFour,
-    doubler,
-    combineArrays,
-    wordWithinArray,
-    echo,
-    fizzBuzz,
-    hello,
-    goodbye,
-    isFive,
-    isOdd,
-    isSubString,
-    aCounter   
+  divideByThree,
+  averageOfTwo,
+  averageOfFour,
+  doubler,
+  combineArrays,
+  wordWithinArray,
+  echo,
+  fizzBuzz,
+  hello,
+  goodbye,
+  isFive,
+  isOdd,
+  isSubString,
+  aCounter
 }
